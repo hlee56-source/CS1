36edd0127409e29bfa45ee8a0e382176905672b0
Harrison Lee <1019920@lcps.org>
2026-09-04 10:26:31 -0400

    fixed robot karel

---
+++ RobotKarel/src/RobotTest.java
@@ -0,0 +1,11 @@
+import edu.fcps.karel2.Display;
+import edu.fcps.karel2.Robot;
+
+public class RobotTest {
+    public static void main(String[] args) {
+        Display.openWorld("maps/ACL.map");
+        Display.setSize(10,10);
+
+        Robot billy = new Robot();
+    }
+}

563cf3993c8a7d3054e5d6563c718d550ac15242
Harrison Lee <1019920@lcps.org>
2026-09-11 12:55:24 -0400

    added lab 2 parts 1 and 2

---
+++ RobotKarel/src/Athlete.java
@@ -0,0 +1,22 @@
+import edu.fcps.karel2.Robot;
+
+public class Athlete extends Robot {
+    public Athlete(int x, int y, int direction, int beepers) {
+        super(x, y, direction, beepers);
+    }
+    void moveAndPlaceNum(int amount){
+        for (int i = 0; i < amount; i++) {
+            putBeeper();
+            move();
+        }
+    }
+    public void turnRight() {
+        turnLeft();
+        turnLeft();
+        turnLeft();
+    }
+    public void turnAround() {
+        turnLeft();
+        turnLeft();
+    }
+}
--- RobotKarel/src/RobotTest.java
+++ RobotKarel/src/RobotTest.java
@@ -3,10 +3,26 @@ import edu.fcps.karel2.Robot;
 
 public class RobotTest {
     public static void main(String[] args) {
-        Display.openWorld(".../maps/ACL.map");
+        Display.openWorld("../maps/ACL.map");
         Display.setSize(10,10);
-
-        Jumper billy = new Jumper();
-        billy.jump();
+        Athlete billy = new Athlete(1, 1, 1, 6);
+        billy.turnRight();
+        billy.moveAndPlaceNum(3);
+        billy.turnLeft();
+        billy.move();
+        billy.turnLeft();
+        billy.move();
+        billy.moveAndPlaceNum(2);
+        billy.turnRight();
+        billy.move();
+        billy.turnRight();
+        billy.move();
+        billy.move();
+        billy.putBeeper();
+        billy.move();
+        billy.turnRight();
+        billy.move();
+        billy.move();
+        billy.turnAround();
     }
 }

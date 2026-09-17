66d2c64205ab19b3408e132e5e8dc5d93f229c67
Harrison Lee <1019920@lcps.org>
2026-09-17 15:23:38 -0400

    edit mountain climber

--- MountainClimber/src/ClimbEveryMountain.java
+++ MountainClimber/src/ClimbEveryMountain.java
@@ -1,32 +1,37 @@
 import edu.fcps.karel2.Display;
 
 public class ClimbEveryMountain {
+    static void getTreasure(Climber x) {
+        x.turnRight();
+        x.putBeeper();
+        x.move();
+        for (int i = 0; i < 4; i++) {
+            x.climbUpRight();
+        }
+        for (int i = 0; i < 3; i++) {
+            x.climbDownLeft();
+        }
+        x.turnAround();
+        for (int i = 0; i < 3; i++) {
+            x.climbUpLeft();
+        }
+        //a.turnAround();
+        for (int i = 0; i < 4; i++) {
+            x.climbDownRight();
+        }
+        x.move();
+        x.putBeeper();
+        x.turnRight();
+        x.move();
+        x.turnAround();
+    }
     static void main() {
         Display.openWorld("G:\\My Drive\\School\\9th grade\\CS1\\JKarel Start Files\\JKarel Start Files\\maps\\mountain.map");
         Display.setSize(17, 17);
         Display.setSpeed(10);
         Climber a = new Climber(8, 1, 1, 10);
-        a.turnRight();
-        a.putBeeper();
-        a.move();
-        for (int i = 0; i < 4; i++) {
-            a.climbUpRight();
-        }
-        for (int i = 0; i < 3; i++) {
-            a.climbDownLeft();
-        }
-        a.turnAround();
-        for (int i = 0; i < 3; i++) {
-            a.climbUpLeft();
-        }
-        //a.turnAround();
-        for (int i = 0; i < 4; i++) {
-            a.climbDownRight();
-        }
-        a.move();
-        a.putBeeper();
-        a.turnRight();
-        a.move();
-        a.turnAround();
+        getTreasure(a);
+        Climber b = new Climber(8, 1, 1, 10);
+        getTreasure(b);
     }
 }
